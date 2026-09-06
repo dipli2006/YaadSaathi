b25a74477bdc797d634260054acf9ce997c2f77c
Divya T <divinagar95@gmail.com>
2026-09-06 15:18:42 +0530

    Initial flutter project setup

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF2F6B5F);
+  static const Color secondary = Color(0xFFF4C95D);
+
+  static const Color background = Color(0xFFF7F7F5);
+
+  static const Color white = Colors.white;
+
+  static const Color black = Color(0xFF222222);
+
+  static const Color grey = Color(0xFF757575);
+
+  static const Color lightGrey = Color(0xFFE5E5E5);
+
+  static const Color success = Color(0xFF4CAF50);
+
+  static const Color error = Color(0xFFE53935);
+}
---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,11 @@
+class AppStrings {
+  static const appName = "YaadSaathi";
+
+  static const english = "English";
+  static const hindi = "हिन्दी";
+
+  static const elderly = "Elderly User";
+  static const caregiver = "Caregiver";
+
+  static const continueText = "Continue";
+}
